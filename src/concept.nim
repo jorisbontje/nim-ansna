1fb4a8caaa5838c2efb5c9b8ba5c611ca6d7def2
Joris Bontje <jorisbontje@gmail.com>
2018-10-01 22:26:33 +0200

    concept

---
+++ src/concept.nim
@@ -0,0 +1,29 @@
+import hashes
+
+import attention
+import sdr
+import task
+import usage
+
+const PRECONDITION_BELIEFS_MAX = 512
+const POSTCONDITION_BELIEFS_MAX = 512
+const EVENT_BELIEFS_MAX = 512
+
+type
+  Concept* = object
+    attention: Attention
+    usage: Usage
+    # name of the concept like in OpenNARS
+    name: SDR
+    nameHash: Hash
+    eventBeliefs: seq[Task]
+    preconditionBeliefs: seq[Task]
+    postconditionBeliefs: seq[Task]
+
+proc initConcept*(name: SDR): Concept =
+    result.name = name
+    result.eventBeliefs = newSeq[Task]()
+    result.preconditionBeliefs = newSeq[Task]()
+    result.postconditionBeliefs = newSeq[Task]()
+    # Generate CRC checksum too:
+    result.nameHash = name.hash
