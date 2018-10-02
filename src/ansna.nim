09284886a90575745372a3e4d26ec645897f84ab
Joris Bontje <jorisbontje@gmail.com>
2018-10-02 16:49:07 +0200

    main()

--- src/ansna.nim
+++ src/ansna.nim
@@ -16,8 +16,14 @@ when isMainModule:
   let conceptAName = encodeTerm(2)
   let conceptA = initConcept(conceptAName)
   mem.addConcept(conceptA)
+  echo conceptA
 
   let closest = mem.findClosestConceptByNameExhaustive(conceptAName)
   echo closest.get()
 
+  # numeric encoder test
+  let w = 40
+  let sdrForNumber = encodeScalar(SDR_size, w, 0, 64, 30)
 
+  echo "SDR for number 30:"
+  echo $sdrForNumber
