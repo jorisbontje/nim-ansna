e1565d85291f00fa038a8732935426d9f12813da
Joris Bontje <jorisbontje@gmail.com>
2018-10-05 13:35:51 +0200

    stamp logic

--- tests/test_stamp.nim
+++ tests/test_stamp.nim
@@ -13,8 +13,8 @@ proc toEvidentalBase(expected: openarray[int]): array[STAMP_size, int] =
 suite "stamp":
 
   setup:
-    var a = initStamp()
-    var b = initStamp()
+    var a = Stamp()
+    var b = Stamp()
 
   test "checkOverlap returns false when both empty":
     check(not checkOverlap(a, b))
@@ -42,32 +42,36 @@ suite "stamp":
     a.evidentalBase = [1].toEvidentalBase
     b.evidentalBase = [1].toEvidentalBase
     let c = make(a, b)
-    check(c.evidentalBase == [1].toEvidentalBase)
+    check(c.evidentalBase == [1, 1].toEvidentalBase)
+    # ??? Shouldn't this be [1]
+    # XXX https://github.com/patham9/ANSNA/issues/21
 
   test "make unequal stamps":
     a.evidentalBase = [1].toEvidentalBase
     b.evidentalBase = [2].toEvidentalBase
     let c = make(a, b)
-    check(c.evidentalBase == [1].toEvidentalBase)
-    # ??? Shouldn't this be [1, 2]
+    check(c.evidentalBase == [1, 2].toEvidentalBase)
 
   test "make b stamps longer":
     a.evidentalBase = [1].toEvidentalBase
     b.evidentalBase = [2, 3].toEvidentalBase
     let c = make(a, b)
-    check(c.evidentalBase == [1, 2].toEvidentalBase)
-    # ??? Shouldn't this be [1, 2, 3]
+    check(c.evidentalBase == [1, 2, 3].toEvidentalBase)
 
   test "make a stamps longer":
     a.evidentalBase = [1, 2].toEvidentalBase
     b.evidentalBase = [3].toEvidentalBase
     let c = make(a, b)
-    check(c.evidentalBase == [1, 2].toEvidentalBase)
-    # ??? Shouldn't this be [1, 2, 3]
+    check(c.evidentalBase == [1, 3, 2].toEvidentalBase)
 
   test "make a and b stamps longer":
     a.evidentalBase = [1, 2].toEvidentalBase
     b.evidentalBase = [3, 4].toEvidentalBase
     let c = make(a, b)
-    check(c.evidentalBase == [1, 3].toEvidentalBase)
-    # ??? Shouldn't this be [1, 2, 3, 4]
+    check(c.evidentalBase == [1, 3, 2, 4].toEvidentalBase)
+
+  test "make stamps truncation":
+      a.evidentalBase = toSeq(1..20).toEvidentalBase
+      b.evidentalBase = toSeq(40..59).toEvidentalBase
+      let c = make(a, b)
+      check(c.evidentalBase == [1, 40, 2, 41, 3, 42, 4, 43, 5, 44, 6, 45, 7, 46, 8, 47, 9, 48, 10, 49].toEvidentalBase)
